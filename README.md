de66542cab1bb38d20ae9f6faae058b5f720ab7c
root <root@chipr07-I10500.ca.com>
2018-02-03 08:24:38 -0500

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# learn-chef
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/learn-chef/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: learn-chef
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
